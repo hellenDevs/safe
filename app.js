d3ffb39b4172f309b8fe77ac6c37d3b19bc8d8e4
hellenDevs <167775571+hellenDevs@users.noreply.github.com>
2024-11-18 04:35:40 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// Handle button click for verification
+function verify() {
+  // Redirect to another page
+  window.location.href = "/teleg.html"; // Replace with your desired URL
+  Telegram.WebApp.close(); // Close the mini-app in Telegram
+}
+
